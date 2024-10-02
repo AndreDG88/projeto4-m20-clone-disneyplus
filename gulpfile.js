486a9c2b59ca23d5aea637a7fb0c7dc68e22e831
André Soares <andre.soares.designer@gmail.com>
2024-10-01 21:04:26 -0300

    criação do hero e adicionando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
+// links para plugins e recursos
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// configuração de funções
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+// Configuração de comandos de exportação
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
