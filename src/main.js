48c4b8e8ecd367ddd64b656be2c33abeeb9a3fa4
André Soares <andre.soares.designer@gmail.com>
2024-10-04 22:19:23 -0300

    adicionando o Rodapé ao site

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // seletor para abas de conteúdo
     const buttons = document.querySelectorAll('[data-tab-button]');
+    // seletor para as sessões de perguntas do faq
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    // Função para as abas de conteúdo
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +15,19 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    // função para controle das abas do faq
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
